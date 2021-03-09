cb7935261aefb97213f5711af746310d310ae9ca
Nog <edrogonzales@hotmail.com>
2021-03-08 17:40:50 -0800

    Cleaned out website design. Final implementations for the while

--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -6,12 +6,16 @@ export const SliderData = [
     title: "Happy Bonsai",
     video: bonsaiPreview,
     link: "https://happy-bonsai.netlify.app/",
-    description: "Happy Bonsai Description",
+    github: "https://github.com/Nog-bs/happy-bonsai",
+    description:
+      "Project I built for the final stages of my Web Development program at BrainStation. I wanted to start reading more and an app to reflect that growth seemed like a particularly fun venture to try. It is a full-stack application with CRUD functionality, user authentication, and 3D rendering implemented.",
   },
   {
     title: "Amay Sweets",
     video: amayPreview,
     link: "https://amay-sweets.com/",
-    description: "Amay Sweets Description",
+    github: "https://github.com/Nog-bs/amay-sweets",
+    description:
+      "Landing page for a local baking business that I built with a colleague of mine. The goal was to create a nice and sleek page for a local baking business. The application required using Git and some collaboration practices we learned from BrainStation.",
   },
 ];
--- src/pages/CodingPage.js
+++ src/pages/CodingPage.js
@@ -2,9 +2,15 @@
 import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import HoverVideoPlayer from "react-hover-video-player";
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+import {
+  FaArrowAltCircleRight,
+  FaArrowAltCircleLeft,
+  FaArrowRight,
+} from "react-icons/fa";
 import { FcHome } from "react-icons/fc";
-import { FaArrowRight } from "react-icons/fa";
+import { SiLinkedin, SiInstagram } from "react-icons/si";
+import { VscGithub } from "react-icons/vsc";
+import { AiOutlineMail } from "react-icons/ai";
 // COMPONENTS
 
 // STYLE
@@ -12,19 +18,24 @@ import "../styles/pages/CodingPage.scss";
 // ASSETS
 import resume from "../assets/download/edro-resume.pdf";
 import { SliderData } from "../data/SliderData";
+import javascript from "../assets/images/js.png";
+import reactIcon from "../assets/images/react.png";
+import styledComponents from "../assets/images/styled-components.png";
+import sass from "../assets/images/sass.png";
+import git from "../assets/images/git.png";
 
 const CodingPage = () => {
   // STATES & VARIABLES
   const [current, setCurrent] = useState(0);
   const length = SliderData.length;
-
   // VIDEO REFS
   const websiteRef = useRef();
   const amayRef = useRef();
-
   // IMAGESLIDER LOGIC
   const nextSlide = () => setCurrent(current === length - 1 ? 0 : current + 1);
-  const prevSlide = () => setCurrent(current === length ? 0 : current - 1);
+  const prevSlide = () =>
+    setCurrent(current < 0 || current === length ? 0 : current - 1);
+  console.log(current);
 
   // DATE TIME FUNCTION
   const dateStart = () => {
@@ -45,9 +56,6 @@ const CodingPage = () => {
       <div id="style-3" className="coding__section">
         <div className="coding__head">
           <h2 className="coding__head-title">Full Stack Developer</h2>
-          <a className="coding__head-download" href={resume} download>
-            Download Resume
-          </a>
         </div>
         <div className="coding__about">
           <h3 className="coding__about-head">About</h3>
@@ -63,7 +71,36 @@ const CodingPage = () => {
             </p>
           </ul>
         </div>
+        <div className="coding__skill">
+          <h3 className="coding__skill-head">Technical Toolkit</h3>
+          <ul className="coding__skill-kit">
+            <img
+              alt="javascript"
+              src={javascript}
+              className="coding__skill-kit-item"
+            />
+            <img
+              alt="react"
+              src={reactIcon}
+              className="coding__skill-kit-item"
+            />
+            <img
+              alt="styled components"
+              src={styledComponents}
+              className="coding__skill-kit-item"
+            />
+            <img alt="sass" src={sass} className="coding__skill-kit-item" />
+            <img alt="git" src={git} className="coding__skill-kit-item" />
+          </ul>
+          <div className="coding__skill-note">
+            <p className="coding__skill-note-text">
+              To see the rest of my toolkit
+            </p>
+            <button className="coding__skill-note-btn">Download Resume</button>
+          </div>
+        </div>
         <div className="coding__project">
+          <h3 className="coding__project-head">Portfolio</h3>
           <FaArrowAltCircleLeft className="project__left" onClick={prevSlide} />
           <FaArrowAltCircleRight
             className="project__right"
@@ -78,7 +115,7 @@ const CodingPage = () => {
                 {index === current && (
                   <>
                     <h4 className="project__head">{slide.title}</h4>
-                    {/* <p className="project__description">{slide.description}</p> */}
+                    <p className="project__description">{slide.description}</p>
                     <div className="project__video">
                       <HoverVideoPlayer
                         videoSrc={slide.video}
@@ -87,6 +124,13 @@ const CodingPage = () => {
                           width: "75%",
                         }}
                       />
+                      <a
+                        className="project__video-link"
+                        target="_blank"
+                        href={slide.github}
+                      >
+                        <VscGithub className="project__video-github" />
+                      </a>
                     </div>
                     <a
                       className="project__link"
@@ -101,6 +145,42 @@ const CodingPage = () => {
             );
           })}
         </div>
+        <footer className="coding__footer">
+          <div className="coding__footer-info">
+            <a
+              href="https://www.linkedin.com/in/edrogonzales/"
+              target="_blank"
+              className="coding__footer-link"
+            >
+              <SiLinkedin className="coding__footer-icon" />
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/Nog-bs"
+              className="coding__footer-link"
+            >
+              <VscGithub className="coding__footer-icon" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.instagram.com/nog.e/"
+              className="coding__footer-link"
+            >
+              <SiInstagram className="coding__footer-icon" />
+            </a>
+            <a
+              target="_blank"
+              href="mailto:edrogonzales@gmail.com"
+              className="coding__footer-link"
+            >
+              <AiOutlineMail className="coding__footer-icon" />
+            </a>
+
+            <a className="coding__footer-download" href={resume} download>
+              Download Resume
+            </a>
+          </div>
+        </footer>
       </div>
     </section>
   );
